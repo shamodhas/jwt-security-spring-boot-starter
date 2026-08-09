fb144a143ee25f3ac7b220578b703a8f51a4928a
shamodhas <shamodha7@gmail.com>
2026-08-09 14:42:02 +0530

    add MIT License

--- src/main/java/com/shamodha/jwtsecurityspringbootstarter/security/AuthenticationProvider.java
+++ src/main/java/com/shamodha/jwtsecurityspringbootstarter/security/AuthenticationProvider.java
@@ -2,6 +2,18 @@ package com.shamodha.jwtsecurityspringbootstarter.security;
 
 import com.shamodha.jwtsecurityspringbootstarter.model.UserContext;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AuthenticationProvider {
-    UserContext authenticate(String username, String password);
+    Optional<UserCredential> loadUserByUsername(String username);
+
+    record UserCredential(
+            String userId,
+            String username,
+            String email,
+            String encodedPassword,
+            List<String> roles
+    ) {
+    }
 }
