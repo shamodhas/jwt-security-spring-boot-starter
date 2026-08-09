c9aabc1acf2cfef4698a0b2c1c34ed7983806a29
shamodhas <shamodha7@gmail.com>
2026-08-09 11:11:35 +0530

    initial commit

---
+++ src/main/java/com/oc/jwtsecurityspringbootstarter/JwtSecuritySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.oc.jwtsecurityspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecuritySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecuritySpringBootStarterApplication.class, args);
+    }
+
+}
